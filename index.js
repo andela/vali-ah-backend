27cbace4ea8c7cd39bfa21d304478c16d13efceb
Nenny Dike <nennydike@gmail.com>
2019-07-23 12:42:27 +0100

    chore(refactor) : refactor server and folder structure
    - restructure the folder on the vali-ah application
    - edit Index.js file
    - add script for development and for production
    - uninstall unwanted packages
    
    [Starts #167452886]

--- index.js
+++ index.js
@@ -1,93 +1,70 @@
-const fs = require("fs"),
-    http = require("http"),
-    path = require("path"),
-    methods = require("methods"),
-    express = require("express"),
-    bodyParser = require("body-parser"),
-    session = require("express-session"),
-    cors = require("cors"),
-    passport = require("passport"),
-    errorhandler = require("errorhandler"),
-    mongoose = require("mongoose");
+/* eslint-disable no-console */
+import '@babel/polyfill';
+import errorhandler from 'errorhandler';
+import express, { json, urlencoded } from 'express';
+import { config } from 'dotenv';
+import cors from 'cors';
+import routes from './routes';
 
-const isProduction = process.env.NODE_ENV === "production";
+config();
 
-// Create global app object
-const app = express();
+const { PORT } = process.env; // setup port to be used
+const app = express(); // calling an instance of express
 
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cors());
 
-// Normal express config defaults
-app.use(require("morgan")("dev"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
-
-app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
-);
-
-if (!isProduction) {
-    app.use(errorhandler());
-}
-
-if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
-} else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
-}
-
-require("./models/User");
-
-app.use(require("./routes"));
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+// index route
+app.get('/', (request, response) => {
+  response.status(200).send('1kbIdeas');
 });
 
-/// error handlers
+app.use('/api/v1', routes);
+
+// catch 404 and forward to error handler
+app.use('*', (request, response) => {
+  response.status(404).send('Not Found');
+});
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+
+// error handlers
 
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-    app.use(function(err, req, res, next) {
-        console.log(err.stack);
+  app.use((err, req, res) => {
+    console.log(err.stack);
 
-        res.status(err.status || 500);
+    res.status(err.status || 500);
 
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
 
-// finally, let's start our server...
-const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
-});
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+export default app;
